2a5585f7d184ecfd32dceee18100f494e614350b
r-ex <67599507+r-ex@users.noreply.github.com>
2026-05-29 14:43:09 +0100

    validation: add more rules

--- src/core/filehandling/validation.cpp
+++ src/core/filehandling/validation.cpp
@@ -3,23 +3,96 @@
 #include <game/asset.h>
 #include <game/rtech/cpakfile.h>
 
+
+#include <game/rtech/assets/settings_layout.h>
+#include <game/rtech/assets/shader.h>
+#include <game/rtech/assets/shaderset.h>
+#include <game/rtech/assets/subtitles.h>
+#include <game/rtech/assets/texture.h>
+#include <game/rtech/assets/texture_anim.h>
+#include <game/rtech/assets/texture_list.h>
+
 struct PakAssetValidationValues_t
 {
 	// Version -> Vector of known header sizes
-	std::map<int, std::vector<int>> knownVersions;
+	std::unordered_map<int, std::vector<int>> knownVersions;
 	
-	bool expectStarpakOffset : 1;
-	bool requireStarpakOffset : 1;
+	bool expectStarpakOffset : 1;  // Expect that the asset type can have a starpak offset
+	bool requireStarpakOffset : 1; // Require that the asset type has a starpak offset
 
-	bool expectOptStarpakOffset : 1;
-	bool requireOptStarpakOffset : 1;
+	bool expectOptStarpakOffset : 1;  // Expect that the asset type can have an opt starpak offset
+	bool requireOptStarpakOffset : 1; // Require that the asset type has an opt starpak offset
 
-	bool expectDataPage : 1;
-	bool requireDataPage : 1;
+	bool expectDataPage : 1; // Expect that the asset type can have a data (cpu) page
+	bool requireDataPage : 1; // Require that the asset type has a data (cpu) page
+
+	PakAssetValidationValues_t() : knownVersions(),
+		expectStarpakOffset(false), requireStarpakOffset(false),
+		expectOptStarpakOffset(false), requireOptStarpakOffset(false),
+		expectDataPage(false), requireDataPage(false) {
+	};
 };
+
+static std::map<AssetType_t, PakAssetValidationValues_t> VALIDATION_DATA_BANK = {
+};
+
+// Now this is going to be one veeeery big function...
+void CreateValidationDataBank()
+{
+	auto& v = VALIDATION_DATA_BANK;
+
+	// Settings Layout
+	auto& stlt = v[AssetType_t::STLT];
+	stlt.knownVersions[0] = { sizeof(SettingsLayoutHeader_v0_t) };
+
+	// Shader
+	auto& shdr = v[AssetType_t::SHDR];
+	shdr.knownVersions[8] = { sizeof(ShaderAssetHeader_v8_t) };
+	shdr.knownVersions[12] = { sizeof(ShaderAssetHeader_v12_t) };
+	shdr.knownVersions[13] = { sizeof(ShaderAssetHeader_v13_t) };
+	shdr.knownVersions[14] = { sizeof(ShaderAssetHeader_v14_t) };
+	shdr.knownVersions[15] = { sizeof(ShaderAssetHeader_v15_t) };
+	shdr.knownVersions[16] = { sizeof(ShaderAssetHeader_v15_t) };
+	shdr.knownVersions[17] = { sizeof(ShaderAssetHeader_v15_t) };
+	shdr.expectDataPage = true;
+
+	// Shader Set
+	auto& shds = v[AssetType_t::SHDS];
+	shds.knownVersions[8] = { sizeof(ShaderSetAssetHeader_v8_t) };
+	shds.knownVersions[11] = { sizeof(ShaderSetAssetHeader_v11_t) };
+	shds.knownVersions[12] = { sizeof(ShaderSetAssetHeader_v12_t) };
+	shds.knownVersions[13] = { sizeof(ShaderSetAssetHeader_v12_t), sizeof(ShaderSetAssetHeader_v13_t) };
+	shds.knownVersions[14] = { sizeof(ShaderSetAssetHeader_v14_t) };
+	shds.knownVersions[15] = { sizeof(ShaderSetAssetHeader_v15_t) };
+
+	// Subtitles
+	auto& subt = v[AssetType_t::SUBT];
+	subt.knownVersions[0] = { sizeof(SubtitlesAssetHeader_v0_t) };
+	subt.knownVersions[1] = { sizeof(SubtitlesAssetHeader_v0_t) };
+
+	// Texture
+	auto& txtr = v[AssetType_t::TXTR];
+	txtr.expectStarpakOffset = true;
+	txtr.expectOptStarpakOffset = true;
+	txtr.expectDataPage = true;
+	txtr.requireDataPage = true;
+
+	txtr.knownVersions[8] = { sizeof(TextureAssetHeader_v8_t) };
+	txtr.knownVersions[9] = { sizeof(TextureAssetHeader_v9_t) };
+	txtr.knownVersions[10] = { sizeof(TextureAssetHeader_v10_t) };
+
+	// Texture Animation
+	auto& txan = v[AssetType_t::TXAN];
+	txan.knownVersions[1] = { sizeof(TextureAnimAssetHeader_v1_t) };
+
+	// Texture List
+	auto& txls = v[AssetType_t::TXLS];
+	txls.knownVersions[1] = { sizeof(TextureListHeader_v1_s) };
+}
 
 bool ValidateLoadedPakFiles()
 {
+	CreateValidationDataBank();
 	printf("\nVALIDATION: Checking %lld assets across %lld files\n", g_assetData.GetNumAssets(), g_assetData.GetNumContainers());
 
 	uint32_t numSegmentErrors = 0;
@@ -56,7 +129,7 @@ bool ValidateLoadedPakFiles()
 	}
 
 	const uint32_t numContainerErrors = g_assetData.m_numFailedContainerLoads + numSegmentErrors + numAssetErrors;
-	printf("\tFound %i problems:\n\t%u segment padding errors, %u asset data errors, %u files failed to load\n", numContainerErrors, numSegmentErrors, numAssetErrors, g_assetData.m_numFailedContainerLoads);
+	printf("Found %i problems:\n\t%u segment padding errors, %u asset data errors, %u files failed to load\n", numContainerErrors, numSegmentErrors, numAssetErrors, g_assetData.m_numFailedContainerLoads);
 
 	return true;
 }
