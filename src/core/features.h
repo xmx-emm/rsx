5cba4581ef9ea7c3ca4926105b56bcab3ff63f87
r-ex <67599507+r-ex@users.noreply.github.com>
2026-06-24 14:47:13 +0100

    Update features.h

--- src/core/features.h
+++ src/core/features.h
@@ -2,12 +2,15 @@
 
 // [ASSET FEATURES]
 #define HAS_ODL_ASSET
-#define HAS_QC // unsure if QC is ready for release yet, so for now it's off!
+#define HAS_QC
+//#define HAS_BSP_SUPPORT
 
 // [GENERAL FEATURES]
-//#define HAS_ITEMFLAV_WINDOW
+#define HAS_ITEMFLAV_WINDOW
 //#define ADVANCED_MODEL_PREVIEW
+//#define HAS_BONED_MODELS
 
 // [DEBUG FEATURES]
 //#define DEBUG_NO_ASEQ_POSTLOAD // - DEBUG ONLY - disables (very) slow postloading for animseq assets
 //#define DEBUG_IMGUI_DEMO       // - DEBUG ONLY - compiles in a call to ImGui::ShowDemoWindow
+//#define DEBUG_LOAD_SHADERS_DISK
